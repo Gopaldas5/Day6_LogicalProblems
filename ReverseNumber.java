11c5d578962689be411c0ed40c8418960e8ac2e2
Gopal kumar Das <35631909+Gopaldas5@users.noreply.github.com>
2022-07-04 21:21:50 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class ReverseNumber {
     public static void main(String[] args) {
         int num = 0;
-        int reversenum = 0;
+        int reverseNum = 0;
         System.out.println("Enter the number:- ");
-        Scanner in = new Scanner(System.in);
-        num = in.nextInt();
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
         while( num != 0 )
         {
-            reversenum = reversenum * 10;
-            reversenum = reversenum + num%10;
+            reverseNum = reverseNum * 10;
+            reverseNum = reverseNum + num % 10;
             num = num/10;
         }
 
-        System.out.println("Reverse of input number is: "+reversenum);
+        System.out.println("Reverse of input number is:- "+reversenum);
     }
 }
