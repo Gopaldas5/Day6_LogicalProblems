4f4b2fe4e1fc273d92d97485c49f9efeb2812e31
Gopal kumar Das <35631909+Gopaldas5@users.noreply.github.com>
2022-07-04 21:12:56 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -10,9 +10,9 @@ public class Fibonacci {
             int b = 0;
             int c = 1;
             Scanner s = new Scanner(System.in);
-            System.out.print("Enter value of n:");
+            System.out.print("Enter value of n:- ");
             n = s.nextInt();
-            System.out.print("Fibonacci Series:");
+            System.out.print("Fibonacci Series:- ");
             for(int i = 1; i <= n; i++)
             {
                 a = b;
